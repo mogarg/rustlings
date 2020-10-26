f88ab6c2db4d21e214ceb2caf113ccddf21c0c0e
Mohit Garg <mohitce@vt.edu>
2020-10-26 15:33:29 -0400

    solve variables exercises (#1)
    * Solved exercises under variables
    
    * Added description to the solutions
    
    Co-authored-by: Mohit Garg <mgarg51@bloomberg.net>

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,11 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
     let number = "T-H-R-E-E";
     println!("Spell a Number : {}", number);
-    number = 3;
+    let number = 3; // shadowing; although can't reuse x without mut
+                    // we can redeclare x
+                    // we can also redeclare x with a different type
     println!("Number plus two is : {}", number + 2);
 }
