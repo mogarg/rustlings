78f4d9dafe0084f979aa2734f18ce73dba9f9ccb
Mohit Garg <mohitce@vt.edu>
2020-10-31 00:59:26 -0400

    Solved enums (#7)

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,12 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move { x: i32, y: i32 },    // struct like enum
+    Echo(String),               // tuple like enum
+    ChangeColor(i32, i32, i32), // tuple like `enum`
+    Quit,
 }
 
 impl Message {
